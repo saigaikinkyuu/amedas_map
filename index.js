fe157b8b6fc9d8e75633da72519f5c6b49597da6
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-05-03 21:47:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -61,13 +61,12 @@ $.getJSON("https://www.jma.go.jp/bosai/amedas/data/map/"+new Date().getFullYear(
           var rgba = "rgba(170,0,199,0.3)"
         }
         var markerL = new L.LatLng(latitude, longitude);
-        var markerIcon = L.circleMarker({
+        var markerIcon = L.circleMarker(markerL,{
           radius: 10, // 半径を設定
           color: color, // 外側の色を赤に設定
           fillColor: rgba, // 中心の色を透明な赤に設定
           fillOpacity: 1 // 塗りつぶしの透明度を設定
-        });
-        var marker = L.marker(markerL, { icon: markerIcon }).addTo(map);
+        }).addTo(map);
         // 地図にマーカーを追加
         marker.addTo(map);
         marker.bindPopup(pref.Name+city.Name,{closeButton: false, zIndexOffset: 10000, maxWidth: 10000})
