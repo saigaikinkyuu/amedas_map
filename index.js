5efe941e68006b3ec7f90a8393afb9febbb7b0b4
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-05-04 10:39:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,72 +14,82 @@ function mapDraw(num) {
         imperial: false
     }).addTo(map);
 
-    // GeoJSON ファイルを地図に追加する地図タイルレイヤーを作成
-    var geoJSONLayer = L.tileLayer('./prefectures.geojson').addTo(map);
-
-    $.getJSON("https://www.jma.go.jp/bosai/amedas/data/map/" + new Date().getFullYear() + ("0" + (new Date().getMonth() + 1)).slice(-2) + ("0" + new Date().getDate()).slice(-2) + ("0" + new Date().getHours()).slice(-2) + "0000.json", function (datas) {
-        $.getJSON("https://www.jma.go.jp/bosai/amedas/const/amedastable.json", function (data) {
-            function formatDate(date) {
-                var year = date.getFullYear();
-                var month = ('0' + (date.getMonth() + 1)).slice(-2);
-                var day = ('0' + date.getDate()).slice(-2);
-                var hours = ('0' + date.getHours()).slice(-2);
-                var minutes = ('0' + date.getMinutes()).slice(-2);
-                var seconds = ('0' + date.getSeconds()).slice(-2);
-                return year + '/' + month + '/' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+    // GeoJSON データを読み込んで地図に追加
+    $.getJSON("./prefectures.geojson", function (data) {
+        L.geoJson(data, {
+            style: {
+                "color": "#ffffff",
+                "weight": 1.5,
+                "opacity": 1,
+                "fillColor": "#3a3a3a",
+                "fillOpacity": 1
             }
+        }).addTo(map);
 
-            for (var key in datas) {
-                function dmsToDd(degrees, minutes, seconds, direction) {
-                    var dd = degrees + minutes / 60 + seconds / (60 * 60);
-                    if (direction == "S" || direction == "W") {
-                        dd = dd * -1;
-                    }
-                    return dd;
+        // AMeDAS データを読み込み、円を追加
+        $.getJSON("https://www.jma.go.jp/bosai/amedas/data/map/" + new Date().getFullYear() + ("0" + (new Date().getMonth() + 1)).slice(-2) + ("0" + new Date().getDate()).slice(-2) + ("0" + new Date().getHours()).slice(-2) + "0000.json", function (datas) {
+            $.getJSON("https://www.jma.go.jp/bosai/amedas/const/amedastable.json", function (data) {
+                function formatDate(date) {
+                    var year = date.getFullYear();
+                    var month = ('0' + (date.getMonth() + 1)).slice(-2);
+                    var day = ('0' + date.getDate()).slice(-2);
+                    var hours = ('0' + date.getHours()).slice(-2);
+                    var minutes = ('0' + date.getMinutes()).slice(-2);
+                    var seconds = ('0' + date.getSeconds()).slice(-2);
+                    return year + '/' + month + '/' + day + ' ' + hours + ':' + minutes + ':' + seconds;
                 }
-
-                var latitude = dmsToDd(data[key].lat[0], (data[key].lat[1] + "0").slice(0, 2), (data[key].lat[1] + "0").slice(2), "N");
-                var longitude = dmsToDd(data[key].lon[0], (data[key].lon[1] + "0").slice(0, 2), (data[key].lon[1] + "0").slice(2), "E");
-
-                if (datas[key].temp) {
-                    if (datas[key].temp[0] <= 0) {
-                        var color = "blue"
-                        var rgba = "rgba(0,0,255,0.3)"
-                    } else if (datas[key].temp[0] <= 10) {
-                        var color = "yellow"
-                        var rgba = "rgba(255,255,0,0.3)"
-                    } else if (datas[key].temp[0] <= 20) {
-                        var color = "orange"
-                        var rgba = "rgba(255,165,0,0.3)"
-                    } else if (datas[key].temp[0] <= 30) {
-                        var color = "red"
-                        var rgba = "rgba(255,0,0,0.3)"
-                    } else {
-                        var color = "purple"
-                        var rgba = "rgba(170,0,199,0.3)"
+                for (var key in datas) {
+                    function dmsToDd(degrees, minutes, seconds, direction) {
+                        var dd = degrees + minutes / 60 + seconds / (60 * 60);
+                        if (direction == "S" || direction == "W") {
+                            dd = dd * -1;
+                        }
+                        return dd;
                     }
-                    var markerL = new L.LatLng(latitude, longitude);
-                    var marker = L.circleMarker(markerL, {
-                        radius: 10,
-                        color: color,
-                        fillColor: rgba,
-                        fillOpacity: 1
-                    }).addTo(map);
-                    marker.bindPopup(data[key].kjName + "(" + data[key].knName + ")<br>" + datas[key].temp[0], {
-                        closeButton: false,
-                        zIndexOffset: 10000,
-                        maxWidth: 10000
-                    });
-                    marker.on('mouseover', function (e) {
-                        this.openPopup();
-                    });
-                    marker.on('mouseout', function (e) {
-                        this.closePopup();
-                    });
+
+                    var latitude = dmsToDd(data[key].lat[0], (data[key].lat[1] + "0").slice(0, 2), (data[key].lat[1] + "0").slice(2), "N");
+                    var longitude = dmsToDd(data[key].lon[0], (data[key].lon[1] + "0").slice(0, 2), (data[key].lon[1] + "0").slice(2), "E");
+
+                    if (datas[key].temp) {
+                        if (datas[key].temp[0] <= 0) {
+                            var color = "blue"
+                            var rgba = "rgba(0,0,255,0.3)"
+                        } else if (datas[key].temp[0] <= 10) {
+                            var color = "yellow"
+                            var rgba = "rgba(255,255,0,0.3)"
+                        } else if (datas[key].temp[0] <= 20) {
+                            var color = "orange"
+                            var rgba = "rgba(255,165,0,0.3)"
+                        } else if (datas[key].temp[0] <= 30) {
+                            var color = "red"
+                            var rgba = "rgba(255,0,0,0.3)"
+                        } else {
+                            var color = "purple"
+                            var rgba = "rgba(170,0,199,0.3)"
+                        }
+                        var markerL = new L.LatLng(latitude, longitude);
+                        var marker = L.circleMarker(markerL, {
+                            radius: 10,
+                            color: color,
+                            fillColor: rgba,
+                            fillOpacity: 1
+                        }).addTo(map);
+                        // 地図にマーカーを追加
+                        marker.bindPopup(data[key].kjName + "(" + data[key].knName + ")<br>" + datas[key].temp[0], {
+                            closeButton: false,
+                            zIndexOffset: 10000,
+                            maxWidth: 10000
+                        });
+                        marker.on('mouseover', function (e) {
+                            this.openPopup();
+                        });
+                        marker.on('mouseout', function (e) {
+                            this.closePopup();
+                        });
+                    }
                 }
-            }
+            });
         });
-
     });
 }
 
