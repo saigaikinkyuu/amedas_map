4e2a77ef7105408fe4a8378357e02e5bfa7b980e
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-05-04 10:00:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,6 @@ var PolygonLayer_Style_nerv = {
     "fillOpacity": 1
 }
 
-$.getJSON("./prefectures.geojson", function (data) {
-    L.geoJson(data, {
-        style: PolygonLayer_Style_nerv
-    }).addTo(map);
-});
 $.getJSON("https://www.jma.go.jp/bosai/amedas/data/map/"+new Date().getFullYear()+""+("0"+(new Date().getMonth()+1)).slice(-2)+""+("0"+new Date().getDate()).slice(-2)+""+("0"+new Date().getHours()).slice(-2)+""+"0000.json", function (datas) {
     $.getJSON("https://www.jma.go.jp/bosai/amedas/const/amedastable.json", function (data) {
     function formatDate(date) {
@@ -74,6 +69,11 @@ $.getJSON("https://www.jma.go.jp/bosai/amedas/data/map/"+new Date().getFullYear(
         marker.on('mouseout', function (e) {this.closePopup();});
       }
       }
+        $.getJSON("./prefectures.geojson", function (data) {
+            L.geoJson(data, {
+                style: PolygonLayer_Style_nerv
+            }).addTo(map);
+        });
 });
     
 })
