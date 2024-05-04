57421ce95b7c760825914b48a4edeaac5cf069ec
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-05-04 16:03:36 +0900

    Update graph.js

--- graph.js
+++ graph.js
@@ -91,5 +91,49 @@ function drawChart(datasets) {
     });
 }
 function content(){
-    //
+    $.getJSON("https://www.jma.go.jp/bosai/amedas/const/amedastable.json", function (data) {
+        var hash = window.location.hash;
+        var pointNumber = hash.substring(1);
+        var namekj = data[pointNumber].kjName
+        var namekn = data[pointNumber].knName
+        document.getElementById("name").innerHTML = namekj + "(" + namekn + ")";
+        var currentTime = new Date();
+        var currentMin = ('0' + currentTime.getMinutes()).slice(-2);
+        var currentHour = currentTime.getHours();
+        var currentYear = currentTime.getFullYear();
+        var currentMonth = ('0' + (currentTime.getMonth() + 1)).slice(-2);
+        var currentDay = ('0' + currentTime.getDate()).slice(-2);
+        var min = 0
+        if(currentMin.slice(0,1) === 0 && currentMin.slice(1,2) <= 5){
+            currentHour -= 1
+            min = 5
+            if(currentHour < 0){
+                currentHour += 23
+                currentDay -= 1
+                if(currentDay <= 0){
+                    currentMonth -= 1
+                    currentDay = new Date(currentYear, currentMonth, 0).getDate();
+                    if(currentMonth <= 0){
+                        currentMonth += 12
+                        currentYear -= 1
+                    }
+                }
+            }
+        }else {
+            min = currentMin.slice(0,1)
+        }
+        $.getJSON("https://www.jma.go.jp/bosai/amedas/data/map/" + currentYear + currentMonth + ("0"+currentDay).slice(-2) + ("0" + hour).slice(-2) + "" + min + "000.json", function (datas) {
+            var temp = datas[pointNumber].temp[0]
+            if(temp.includes('.') === true){
+                var tempResult = ("0" + temp).slice(-4)
+            }else{
+                if(temp.length === 1){
+                    temp = "0" + temp + ".0"
+                }else {
+                    temp = temp + ".0"
+                }
+            }
+            document.getElementById("temp").innerHTML = ("0" + hour).slice(-2) + "時" + min + "0分時点　" + temp + "℃"
+        })
+    })
 }
